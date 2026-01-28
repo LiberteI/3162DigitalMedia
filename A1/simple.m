16b4ad7d360d6c2cda6a0b2379accaa7dee48c6b
LiberteI <liberteix@gmail.com>
2026-01-28 18:42:32 -0400

    added gitignore

--- A1/simple.m
+++ A1/simple.m
@@ -1,8 +1,13 @@
 % simple.m
 
+% symbols with their frequency
 freqs = {'T' 'B' 'R' 'O' 'N' 'E'; 5/24 1/8 1/8 1/3 1/12 1/8};
+
+% generate tree using frequency
 tree = genTree(freqs);
 
+% encode string using tree defined
 code = encode('TOBEORNOTTOBEORTOBEORNOT', tree)
 
+% Decode the encoded string using the generated tree
 stringOut = decode(code, tree)
