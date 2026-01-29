bddf2b6fc246550391d5e9a9342fdb45a32e52e3
LiberteI <liberteix@gmail.com>
2026-01-29 14:39:11 -0400

    added stats on compression rate

--- A1/larger.m
+++ A1/larger.m
@@ -1,29 +1,30 @@
 % larger.m
 
 % read Readme file
-string = fileread('README');
+text = fileread('README');
 
 % prevent error in decoding unprintable characters in ASCII
 %                           NOT ASCII 32 - 126
 % regexprep(origin, substitute, replacement)
-string = regexprep(string, '[^\x20-\x7E]', ' ');
+text = regexprep(text, '[^\x20-\x7E]', ' ');
 
 % generate the Huffman tree from the string
-huffmanTree = genTree(string, "huffman");
+huffmanTree = genTree(text, "huffman");
 
 % generate ShannonFano tree from the string
-shannonFanoTree = genTree(string, "shannon");
+shannonFanoTree = genTree(text, "shannon");
 
 % encode the string using the generated Huffman tree
-huffmanCode = encode(string, huffmanTree);
+huffmanCode = encode(text, huffmanTree);
 
 % encode the string using the generated Shannon-Fano tree
-shannonFanoCode = encode(string, shannonFanoTree);
+shannonFanoCode = encode(text, shannonFanoTree);
 
 % decode the string
 huffmanString = decode(huffmanCode, huffmanTree);
 shannonFanoString = decode(shannonFanoCode, shannonFanoTree);
 
+% print encoding and recovered decoding
 disp("Huffman Encoding");
 disp(huffmanCode);
 
@@ -37,3 +38,21 @@ disp(shannonFanoCode);
 
 disp("Shannon Decoding");
 disp(shannonFanoString);
+
+% assume every char in string is stored 8 bits
+originalBitCount = numel(text) * 8;
+
+% calculate the bit count for Huffman and Shannon-Fano codes
+huffmanBitCount = numel(huffmanCode);
+shannonFanoBitCount = numel(shannonFanoCode);
+
+% calculate compression rate
+huffmanCompressionRate = huffmanBitCount / originalBitCount;
+
+shannonFanoCompressionRate = shannonFanoBitCount / originalBitCount;
+
+disp(" ");
+
+% display compression rates
+fprintf('Huffman Compression Rate: %.4f\n', huffmanCompressionRate);
+fprintf('Shannon-Fano Compression Rate: %.4f\n', shannonFanoCompressionRate);
