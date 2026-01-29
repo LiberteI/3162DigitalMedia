80a0599fa07840845197e380a79b7d6a2696b542
LiberteI <liberteix@gmail.com>
2026-01-28 22:45:23 -0400

    added ShannoFano algo

--- A1/ShannoFano.m
+++ A1/ShannoFano.m
@@ -5,7 +5,44 @@ function tree = ShannoFano(symbols, freqs)
     % when number of symbols == 1
     if numel(symbols) == 1
 
-        % construct node { value, left_subtree, right_subtree } 
+        % construct node { symbol, left_subtree, right_subtree } 
         tree = {symbols{1}, [], []};
         return;
     end
+    
+    % sort frequency descending
+    [freqs, idx] = sort(freqs, 'descend');
+    symbols = symbols(idx);
+    
+    % total frequency
+    totalFreq = sum(freqs);
+    % left collection sum of frequency
+    leftSum = 0;
+
+    % initialise split index to 1
+    splitIdx = 1;
+
+    % 1 based index
+    % add frequency from the first element to total number of elements
+    for i = 1: numel(freqs)
+        leftSum = leftSum + freqs(i);
+        if leftSum >= totaFreq / 2
+            splitIdx = i;
+            break;
+        end
+    end
+
+    % create left sub trees 1 to split idx
+    leftSymbols = symbols(1:splitIdx);
+    leftFreqs = freqs(1:splitIdx);
+
+    % create right sub trees splitIdx + 1 to end
+    rightSymbols = symbols(splitIdx+1:end);
+    rightFreqs = freqs(splitIdx+1:end);
+    
+    % recursively generate more nodes
+    leftTree = ShannoFano(leftSymbols, leftFreqs);
+    rightTree = ShannoFano(rightSymbols, rightFreqs);
+    
+    % construct the tree with subtrees
+    tree = {[leftTree{1}, rightTree{1}], leftTree, rightTree}; 
