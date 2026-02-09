059db3d58e044c46333286cf820e8e5409270785
LiberteI <liberteix@gmail.com>
2026-02-09 14:28:34 -0400

    added threshold dithering function

---
+++ A2/thresholding.m
@@ -0,0 +1,18 @@
+
+% apply thresholding to image with a bit depth of b
+function ditheredImage = thresholding(Image, b)
+    % create 2^b levels
+    levelCount = 2^b;
+    
+    % create 2^b - 1 intervals
+    interval = levelCount - 1;
+
+    % scale the level up so that we can perform round
+    scaledValues = Image * interval;
+
+    % round image
+    roundedImage = round(scaledValues);
+
+    % map rounded values back to the original image range [0. 1]
+    ditheredImage = roundedImage / interval;
+end
