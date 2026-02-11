22c26ce9b7818286aebf007a147ecaa137e62632
LiberteI <liberteix@gmail.com>
2026-02-10 22:56:13 -0400

    added A2 gitignore + floydSteinbergdithering file not completed

--- A2/floydSteinbergDithering.m
+++ A2/floydSteinbergDithering.m
@@ -1,4 +1,18 @@
 function ditheredImage = floydSteinbergDithering(image, b)
+    % define error spread ratios in 4 directions
+    rightRatio = 7 / 16;
+    buttomRightRatio = 1 / 16;
+    buttomRatio = 5 / 16;
+    buttomLeftRatio = 3 / 16;
 
+    % get all pixels in image
+    [height, width] = size(image);
+
+    for y = 1 : height
+        for x = 1 : width
+            fprintf("Pixel at (%d, %d) = %.3f\n", y, x, image(y,x));
+        end
+    end
+    
 
 end
