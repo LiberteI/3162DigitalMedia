db44820ba9d2c842c914ffc2fab9df320ebe5a98
LiberteI <liberteix@gmail.com>
2026-02-12 12:55:23 -0400

    add sanity check

--- A2/task2/main.m
+++ A2/task2/main.m
@@ -1,11 +1,13 @@
 % load audio piano.wav, using int16
-% signals fs
+% x, fs
 [signal, sampleRate] = audioread('piano.wav', 'native');
 
-showSinusoid(signal, sampleRate, 'piano');
 
-% predictor l = 1
+
+
+% ==== predictor l = 1 ====
 coefficients_l1 = [1];
+
 % get prediction error
 predictionError_l1 = predict_signal(signal, coefficients_l1);
 
@@ -13,8 +15,19 @@ outputPath = '/Users/liberte/Desktop/3162/A2/task2/error_l1.wav';
 
 audiowrite(outputPath, int16(predictionError_l1), sampleRate);
 
-% [signal_l1, sampleRate_l1] = audioread('error_l1.wav', 'native');
-showSinusoid(signal_l1, sampleRate_l1, 'error_l1');
+[signal_l1, sampleRate_l1] = audioread('error_l1.wav', 'native');
 
-max(abs(int32(signal)))
-max(abs(int32(predictionError_l1)))
+
+
+
+% ---- sanity check ----
+% max(abs(int32(signal)))
+% max(abs(int32(predictionError_l1)))
+
+% print the first 20 values
+% disp([(1:20)', signal(1:20)])
+% disp([(1:20)', signal_l1(1:20)])
+
+% show sinusoids
+% showSinusoid(signal, sampleRate, 'piano');
+% showSinusoid(signal_l1, sampleRate_l1, 'error_l1');
